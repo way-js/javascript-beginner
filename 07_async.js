afaefc9e94188ad087fb3056f9eda3a4d2fd1ac5
WriteCoin <sovtsardom@gmail.com>
2020-11-29 20:15:08 +0300

    first commit

---
+++ 07_async.js
@@ -0,0 +1,53 @@
+// Event Loop
+
+// const interval = setInterval(() => {
+//     // clearTimeout(timeout)
+//     console.log('After timeout')
+// }, 1000);
+
+// clearInterval(interval)
+
+// const delay = (callback, wait = 1000) => {
+//     setTimeout(callback, wait)
+// }
+
+// delay(() => {
+//     console.log('After 2 seconds')
+// }, 2000)
+
+const delay = (wait = 1000) => {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve()
+            // reject('Что-то пошло не так. Повторите попытку')
+        }, wait)
+    })
+    return promise
+}
+
+// delay(2500)
+//     .then(() => {
+//         console.log('After 2 seconds')
+//     })
+//     .catch( err => console.error('Error:',err))
+//     .finally(() => console.log('Finally'))
+
+const getData = () => new Promise(resolve => resolve([
+    1, 1, 2, 3, 5, 8, 13
+]))
+
+// getData().then(data => console.log(data))
+
+async function asyncExample() {
+    try {
+        await delay(3000)
+        const data = await getData()
+        console.log('Data', data)
+    } catch (e) {
+        console.log(e)
+    } finally {
+        console.log('Finally')
+    }
+}
+
+asyncExample()
